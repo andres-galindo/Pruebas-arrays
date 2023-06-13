6d1f08b59fdfde247e715876351e4a526af77b83
Andres Galindo <99367211+andres-galindo@users.noreply.github.com>
2023-06-12 22:45:59 -0500

    Create script.js
    se agrega la funcionalida al proyecto

---
+++ script.js
@@ -0,0 +1,31 @@
+let modal = document.querySelector(".modal");
+let items = document.querySelectorAll(".items");
+let inombre = document.querySelector(".inombre");
+let bregistro = document.querySelector(".registro");
+let bcancelar = document.querySelector(".cancelar");
+let nombre = document.querySelectorAll(".nombre");
+let bliberar = document.querySelectorAll(".liberar");
+let bcrear = document.querySelectorAll(".crear");
+let indice;
+
+bcrear.forEach((crear, i)=>{
+    bcrear[i].addEventListener("click", ()=>{
+        modal.classList.toggle("activo");
+        indice = i;
+    })
+});
+bregistro.addEventListener("click", ()=>{
+    nombre[indice].innerText = inombre.value;
+    inombre.value = "";
+    modal.classList.toggle("activo");
+});
+
+bliberar.forEach((liberar, i)=>{
+    bliberar[i].addEventListener("click", ()=>{
+        nombre[i].innerText = "";
+    })
+});
+
+bcancelar.addEventListener("click", ()=>{
+    modal.classList.toggle("activo");
+})
