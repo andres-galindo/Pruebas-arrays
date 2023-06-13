97229f1e51e0cae29ec1296d68cb154b0973cd7a
Andres Galindo <99367211+andres-galindo@users.noreply.github.com>
2023-06-12 23:29:32 -0500

    Update script.js
    a

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ bcrear.forEach((crear, i)=>{
     bcrear[i].addEventListener("click", ()=>{
         modal.classList.toggle("activo");
         indice = i;
-        items[i].style.background = "red";
+        items.style.background = "red";
     })
 });
 bregistro.addEventListener("click", ()=>{
